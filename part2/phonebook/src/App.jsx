d6b561a68f9ddfa170d65f8cac1d63339e1508a9
aleenaahmad1 <aleenat31@gmail.com>
2026-01-30 18:17:30 +0200

    ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
   const [showAll, setShowAll] = useState(true)
  
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("promise complete")
+        setPersons(response.data)
+      })
+  }, [])
+
   const handleNewName = (event) => {
     // console.log(event.target.value)
     setNewName(event.target.value)
@@ -28,7 +34,7 @@ const App = () => {
     // console.log(event.target.value)
     setShowAll(false)
     setSearchName(event.target.value)
-    if (event.target.value = ''){
+    if (event.target.value === ''){
       setShowAll(true)
     }
 
