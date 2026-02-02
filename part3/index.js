c3e425e256551028d99e821ccab3e822fff7b099
aleenaahmad1 <aleenat31@gmail.com>
2026-02-02 15:01:25 +0200

    ex 3.4-3.6

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -23,6 +25,15 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+const checkNameExists = (name) => {
+    const person = persons.find(p => p.name === name)
+    if(person){
+        return true
+    } else {
+        return false
+    }
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -47,6 +58,46 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  console.log("delete req")
+  const id = request.params.id
+  console.log("Inside delete request. ID: ", id)
+  
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  console.log("POST request received")
+  const person = request.body
+  console.log(request.body)
+  if(!person.name){
+    return response.status(400).json({
+      error: "name missing"
+    })
+  }
+  if(!person.number){
+    return response.status(400).json({
+      error: "number missing"
+    })
+  }
+  if(checkNameExists(person.name)){
+    return response.status(400).json({
+      error: "Name already exists in the phonebook."
+    })
+  }
+
+  const new_id = Math.floor(Math.random() * 10000) + 1
+  const person_obj = {
+    name: person.name,
+    number: person.number, 
+    id: new_id,
+  }
+  persons = persons.concat(person_obj)
+  response.json(person_obj)
 })
 
 const PORT = 3001
