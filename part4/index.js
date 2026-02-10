5198ed19efcb196750030781ae01d6ec7c222ca1
aleenaahmad1 <aleenat31@gmail.com>
2026-02-10 11:08:50 +0200

    ex 4.1

---
+++ part4/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+// const mongorl = process.env.MONGODB_URI
+const mongoUrl = 'mongodb+srv://fullstack:FullStack@cluster0.xbowfhj.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
