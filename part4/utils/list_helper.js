84a331609a31faad19877e44b14320b368f12d26
aleenaahmad1 <aleenat31@gmail.com>
2026-02-10 11:56:04 +0200

    ex 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,19 @@
 const dummy = (blogs) => {
     return 1
 }
-
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+    if (blogs.length === 0){
+        return 0
+    } else {
+        likes = blogs.map(b => b.likes)
+        console.log("LIKES ARRAY: ", likes)
+        const initial = 0
+        const sum = likes.reduce(
+            (acc, current) => acc + current, 
+            initial
+        )
+        console.log("SUM: ", sum)
+        return sum
+    }
+}
+module.exports = { dummy, totalLikes }
