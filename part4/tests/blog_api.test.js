a873834c6122a85043469d9c5fbb0aa946488df2
aleenaahmad1 <aleenat31@gmail.com>
2026-02-11 12:30:15 +0200

    ex 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -40,10 +40,32 @@ test('all blogs are returned in JSON format', async () => {
   assert.strictEqual(response.body.length, initialBlog.length)
 })
 
-test.only("unique identifier named ID", async () => {
+test("unique identifier named ID", async () => {
     const response = await api.get('/api/blogs')
     object_keys = Object.keys(response.body[0])
     assert(object_keys.includes('id'))
+})
+
+test.only("a new blog can be added", async () => {
+    const newBlog = {
+        title: "blog 3", 
+        author: "qudsia", 
+        url: "blog3.com",
+        likes: '11'
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const blogTitles = response.body.map(b => b.title)
+
+    assert(response.body.length, initialBlog.length + 1)
+    assert(blogTitles.includes('blog 3'))
 })
 
 after(async () => {
