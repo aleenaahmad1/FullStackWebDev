af96f03be4e121f951005210203fb04535181b81
aleenaahmad1 <aleenat31@gmail.com>
2026-02-11 12:47:43 +0200

    ex 4.12

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -31,44 +31,44 @@ beforeEach( async () => {
 
 })
 
-// test('all blogs are returned in JSON format', async () => {
-//   const response = await api
-//   .get('/api/blogs')
-//   .expect(200)
-//   .expect('Content-Type', /application\/json/)
+test('all blogs are returned in JSON format', async () => {
+  const response = await api
+  .get('/api/blogs')
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
 
-//   assert.strictEqual(response.body.length, initialBlog.length)
-// })
+  assert.strictEqual(response.body.length, initialBlog.length)
+})
 
-// test("unique identifier named ID", async () => {
-//     const response = await api.get('/api/blogs')
-//     object_keys = Object.keys(response.body[0])
-//     assert(object_keys.includes('id'))
-// })
+test("unique identifier named ID", async () => {
+    const response = await api.get('/api/blogs')
+    object_keys = Object.keys(response.body[0])
+    assert(object_keys.includes('id'))
+})
 
-// test("a new blog can be added", async () => {
-//     const newBlog = {
-//         title: "blog 3", 
-//         author: "qudsia", 
-//         url: "blog3.com",
-//         likes: '11'
-//     }
+test("a new blog can be added", async () => {
+    const newBlog = {
+        title: "blog 3", 
+        author: "qudsia", 
+        url: "blog3.com",
+        likes: '11'
+    }
 
-//     await api
-//     .post('/api/blogs')
-//     .send(newBlog)
-//     .expect(201)
-//     .expect('Content-Type', /application\/json/)
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
 
-//     const response = await api.get('/api/blogs')
+    const response = await api.get('/api/blogs')
 
-//     const blogTitles = response.body.map(b => b.title)
+    const blogTitles = response.body.map(b => b.title)
 
-//     assert(response.body.length, initialBlog.length + 1)
-//     assert(blogTitles.includes('blog 3'))
-// })
+    assert(response.body.length, initialBlog.length + 1)
+    assert(blogTitles.includes('blog 3'))
+})
 
-test.only("sets no likes to 0", async () => {
+test("sets no likes to 0", async () => {
     const newBlog = {
         title: "blog 3", 
         author: "qudsia", 
@@ -80,8 +80,19 @@ test.only("sets no likes to 0", async () => {
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-    
+
     assert.strictEqual(response.body.likes, 0)
+})
+
+test.only("gives error if no title or url", async () => {
+    const newBlog = {
+        author: "qudsia"   
+    }
+
+    const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
 })
 
 after(async () => {
