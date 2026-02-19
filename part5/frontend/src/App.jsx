c2cd1ce472069ee1bffff7f67ee32144898be58f
aleenaahmad1 <aleenat31@gmail.com>
2026-02-19 14:51:14 +0200

    ex 5.7

--- part5/frontend/src/App.jsx
+++ part5/frontend/src/App.jsx
@@ -58,7 +58,8 @@ const App = () => {
     <h2>blogs</h2>
     <div>
     {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />)}
+        <Blog key={blog.id} blog={blog} />)
+    }
     </div>
     </div>
   )
--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+const Blog = ({ blog }) => {
+
+  const [ showDetails, setShowDetails ] = useState(false)
+  const [ buttonLabel, setButtonLabel ] = useState('view')
+
+  const handleClick = () => {
+    const label = showDetails ? 'view' : 'hide'
+    setShowDetails(!showDetails)
+    setButtonLabel(label)
+  }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}  <button onClick = {handleClick}>{buttonLabel}</button>
+      </div>
+      {
+        showDetails && 
+        (
+          <div>
+            <p>{blog.url}</p>
+            <p>Likes 0</p>
+            <p>{blog.author}</p>
+          </div>
+        )
+      }
+    </div>  
+  )
+
+}
 
 export default Blog
